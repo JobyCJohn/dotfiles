c9a5a9de814c964d0c183e3d12372ab93526aa8d
Joby John <11912680+JobyCJohn@users.noreply.github.com>
2022-01-26 11:03:39 +0530

    fish with tmux config

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,15 @@
+if status is-interactive
+and not set -q TMUX
+    tmux attach || tmux
+end
+
 # initialization
 oh-my-posh --init --shell fish --config ~/.poshthemes/spaceship.omp.json | source
 
 # aliases
-alias vim='nvim'
+alias git='git.exe'
+alias open='wsl-open'
 
-set -g -x fish_greeting
-
+set -gx fish_greeting
+set -gx EDITOR nvim
 set -gx FZF_DEFAULT_OPTS "--height 50% --layout=reverse --inline-info"
